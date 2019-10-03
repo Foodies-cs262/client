e3db5992cd7262cde7f89702243c27a05c72d9d5
AriadustWP <wspark0222@gmail.com>
2019-10-03 16:51:16 -0400

    WIP (lunch)

---
+++ app/src/main/java/edu/calvin/cs262/cheferone/Dinner.java
@@ -0,0 +1,14 @@
+package edu.calvin.cs262.cheferone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dinner);
+    }
+}
