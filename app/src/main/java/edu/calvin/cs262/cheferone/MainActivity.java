4bede3776089cc11e52e017bae4514b4933f2915
wp22 <wp22@maroon06.cs.calvin.edu>
2019-10-04 14:41:46 -0400

    Revert "Merge pull request #13 from Foodies-cs262/AriadustWP/test"
    This reverts commit a0705a19728a996a10da31995682e821892a5e20, reversing
    changes made to 5254f0c3f9e9ca1f72b3d7a77709e3795978c201.

--- app/src/main/java/edu/calvin/cs262/cheferone/MainActivity.java
+++ app/src/main/java/edu/calvin/cs262/cheferone/MainActivity.java
@@ -10,9 +10,6 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     ImageButton myImageButton;
-    ImageButton myImageButton1;
-    ImageButton myImageButton2;
-    ImageButton myImageButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,38 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myImageButton = findViewById(R.id.image_breakfast);
-        myImageButton1 = findViewById(R.id.image_lunch);
-        myImageButton2 = findViewById(R.id.image_dinner);
-        myImageButton3 = findViewById(R.id.image_desert);
 
         myImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentLoadActivity = new Intent (MainActivity.this, Breakfast.class);
-                startActivity(intentLoadActivity);
-            }
-        });
-
-        myImageButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentLoadActivity = new Intent (MainActivity.this, Lunch.class);
-                startActivity(intentLoadActivity);
-            }
-        });
-
-        myImageButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentLoadActivity = new Intent (MainActivity.this, Dinner.class);
-                startActivity(intentLoadActivity);
-            }
-        });
-
-        myImageButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentLoadActivity = new Intent (MainActivity.this, Desert.class);
                 startActivity(intentLoadActivity);
             }
         });
