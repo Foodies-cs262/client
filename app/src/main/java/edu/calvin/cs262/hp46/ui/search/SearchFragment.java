8e3e6d3548dbbbfb7198b8c6d1a5dbc734a17922
AriadustWP <wspark0222@gmail.com>
2019-11-30 17:51:13 -0500

    Random recipe functionality done

--- app/src/main/java/edu/calvin/cs262/hp46/ui/search/SearchFragment.java
+++ app/src/main/java/edu/calvin/cs262/hp46/ui/search/SearchFragment.java
@@ -1,25 +1,19 @@
 package edu.calvin.cs262.hp46.ui.search;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import edu.calvin.cs262.hp46.MainActivity;
 import edu.calvin.cs262.hp46.R;
-import edu.calvin.cs262.hp46.SearchedRecipe;
+import edu.calvin.cs262.hp46.RandomRecipe;
 import edu.calvin.cs262.hp46.SharedViewModel;
 
 public class SearchFragment extends Fragment {
@@ -49,7 +43,7 @@ public class SearchFragment extends Fragment {
 //                myView.setOnClickListener(new View.OnClickListener() {
 //                @Override
 //                public void onClick(View view) {
-//                    Intent intentLoadActivity = new Intent (getActivity(), SearchedRecipe.class);
+//                    Intent intentLoadActivity = new Intent (getActivity(), RandomRecipe.class);
 //                    startActivity(intentLoadActivity);
 //                    model.select("hi");
 //                }
@@ -61,7 +55,7 @@ public class SearchFragment extends Fragment {
         myImageButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Intent intentLoadActivity = new Intent (getActivity(),SearchedRecipe.class);
+            Intent intentLoadActivity = new Intent (getActivity(), RandomRecipe.class);
             startActivity(intentLoadActivity);
             }
         });
